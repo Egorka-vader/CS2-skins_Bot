a43f2843dbba35e37794a8943e8814317c89e5fd
Egorka-vader <egorm3075@gmail.com>
2026-05-08 20:10:37 +0300

    Update keyboard.py

--- app/keyboard.py
+++ app/keyboard.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboardMarkup,KeyboardButton
 
 start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='💎 Premium',callback_data='premium'),InlineKeyboardButton(text="🔪 Оружие",callback_data='guns')],
+    [InlineKeyboardButton(text='💳 Подписка',callback_data='premium'),InlineKeyboardButton(text="🔪 Оружие",callback_data='guns')],
     [InlineKeyboardButton(text='👤 Профиль',callback_data='profile')]
 
 ])
